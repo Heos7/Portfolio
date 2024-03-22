b7886e4a5f7a3063a9dfbbfdc1d275bdb02adc69
Heos7 <maelys.juttier@gmail.com>
2024-03-22 16:58:00 +0100

    Ajout image de fond Motion Design

---
+++ Motion design/Motion Design.js
@@ -0,0 +1,12 @@
+let back = document.getElementById('Back');
+let milieu = document.getElementById('Milieu');
+let front = document.getElementById('Devant');
+let sun = document.getElementById('sun');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    sun.style.left = value + 'px';
+    sun.style.top = value + 'px';
+    Back.style.top = value * 0.5 + 'px';
+    Milieu.style.top = value * 0.25 + 'px';
+})
