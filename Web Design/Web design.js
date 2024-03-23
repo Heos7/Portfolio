cbed75f9c8fef79611cc7660c62e96284259e36c
Heos7 <maelys.juttier@gmail.com>
2024-03-23 15:30:34 +0100

    Finition haut web design

---
+++ Web Design/Web design.js
@@ -0,0 +1,13 @@
+let back = document.getElementById('back');
+let milieu = document.getElementById('milieu');
+let devant = document.getElementById('devant');
+let sun = document.getElementById('sun');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    sun.style.left = value + 'px';
+    sun.style.top = -value *0.25 + 'px';
+    back.style.top = value * 0.5 + 'px';
+    milieu.style.top = value * 0.25 + 'px';
+
+})
