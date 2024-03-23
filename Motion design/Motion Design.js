408f4bed1b95934fd15c4a84e24beb9ab200d9b2
Heos7 <maelys.juttier@gmail.com>
2024-03-23 13:31:47 +0100

    Refonte Accueil

--- Motion design/Motion Design.js
+++ Motion design/Motion Design.js
@@ -6,7 +6,7 @@ let sun = document.getElementById('sun');
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     sun.style.left = value + 'px';
-    sun.style.top = value + 'px';
+    sun.style.top = -value + 'px';
     Back.style.top = value * 0.5 + 'px';
     Milieu.style.top = value * 0.25 + 'px';
 })
