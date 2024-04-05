6b8afca7df019f7604c4b6d55712ec2f2b5f399d
Heos7 <maelys.juttier@gmail.com>
2024-04-05 15:56:23 +0200

    modif animation

--- Accueil/Accueil.js
+++ Accueil/Accueil.js
@@ -2,6 +2,11 @@ let back = document.getElementById('back');
 let milieu = document.getElementById('milieu');
 let front = document.getElementById('devant');
 let sun = document.getElementById('sun');
+let cloud_1 = document.getElementById('nuages-1')
+let cloud_2 = document.getElementById('nuages-2')
+let cloud_3 = document.getElementById('nuages-3')
+let cloud_4 = document.getElementById('nuages-4')
+let cloud_5 = document.getElementById('nuages-5')
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
@@ -9,4 +14,9 @@ window.addEventListener('scroll', function(){
     sun.style.top = value * 1.5 + 'px';
     back.style.top = value * 0.5 + 'px';
     milieu.style.top = value * 0.25 + 'px';
+    cloud_1.style.left = value + 'px';
+    cloud_2.style.left = value + 'px';
+    cloud_3.style.left = value + 'px';
+    cloud_4.style.left = value + 'px';
+    cloud_5.style.left = value + 'px';
 })
